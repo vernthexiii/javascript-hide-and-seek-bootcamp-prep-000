36fd804457004cc57bce65808a30f9bb48712363
Drew D <vernthexiii@gmail.com>
2017-08-31 17:47:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function deepestChild() {
+  var child = document.getElementById('grand-node');
+  var nextChild = child.children[0];
+  while(nextChild) {
+    child = nextChild;
+    nextChild = child.children[0];
+  }
+  return child;
+}
